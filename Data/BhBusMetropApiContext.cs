738672414a86843112313e2fcba1ec6957ecb4dc
Caik Lima <124724369+Caik0@users.noreply.github.com>
2024-08-31 01:15:11 -0300

    Add files via upload

---
+++ Data/BhBusMetropApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BhBusMetropApi.Models;
+
+namespace BhBusMetropApi.Data
+{
+    public class BhBusMetropApiContext : DbContext
+    {
+        public BhBusMetropApiContext (DbContextOptions<BhBusMetropApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BhBusMetropApi.Models.Onibus> Onibus { get; set; } = default!;
+    }
+}
