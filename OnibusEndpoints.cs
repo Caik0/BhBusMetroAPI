d82e89aa7648616e8e10a46ce67bbc7d9a7a617a
Caik Lima <124724369+Caik0@users.noreply.github.com>
2024-08-31 01:13:55 -0300

    Add files via upload

---
+++ OnibusEndpoints.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BhBusMetropApi.Data;
+using BhBusMetropApi.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.OpenApi;
+namespace BhBusMetropApi;
+
+public static class OnibusEndpoints
+{
+    public static void MapOnibusEndpoints (this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/api/Onibus").WithTags(nameof(Onibus));
+
+        group.MapGet("/", async (BhBusMetropApiContext db) =>
+        {
+            return await db.Onibus.ToListAsync();
+        })
+        .WithName("GetAllOnibus")
+        .WithOpenApi();
+
+        group.MapGet("/{id}", async Task<Results<Ok<Onibus>, NotFound>> (int id, BhBusMetropApiContext db) =>
+        {
+            return await db.Onibus.AsNoTracking()
+                .FirstOrDefaultAsync(model => model.Id == id)
+                is Onibus model
+                    ? TypedResults.Ok(model)
+                    : TypedResults.NotFound();
+        })
+        .WithName("GetOnibusById")
+        .WithOpenApi();
+
+        group.MapPut("/onibus/{id}", async (BhBusMetropApiContext db, Onibus updateonibus, int id) =>
+        {
+            var onibus = await db.Onibus.FindAsync(id);
+            if (onibus is null) return Results.NotFound();
+            onibus.Nome = updateonibus.Nome;
+            onibus.Numero = updateonibus.Numero;
+            onibus.Cor = updateonibus.Cor;
+            onibus.Tipo = updateonibus.Tipo;
+            onibus.Peso = updateonibus.Peso;
+            await db.SaveChangesAsync();
+            return Results.NoContent();
+        });
+
+        group.MapPost("/", async (Onibus onibus, BhBusMetropApiContext db) =>
+        {
+            db.Onibus.Add(onibus);
+            await db.SaveChangesAsync();
+            return TypedResults.Created($"/api/Onibus/{onibus.Id}",onibus);
+        })
+        .WithName("CreateOnibus")
+        .WithOpenApi();
+
+        group.MapDelete("/onibus/{id}", async (BhBusMetropApiContext db, int id) =>
+        {
+            var onibus = await db.Onibus.FindAsync(id);
+            if (onibus is null)
+            {
+                return Results.NotFound();
+            }
+            db.Onibus.Remove(onibus);
+            await db.SaveChangesAsync();
+            return Results.Ok();
+        });
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using BhBusMetropApi.Data;
+using BhBusMetropApi;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<BhBusMetropApiContext>(options => options.UseInMemoryDatabase("BHbusdb"));
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapOnibusEndpoints();
+
+app.Run();
