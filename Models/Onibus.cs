acddb3e50844380701d4e0377959e5f75f35a513
Caik Lima <124724369+Caik0@users.noreply.github.com>
2024-08-31 01:14:50 -0300

    Add files via upload

---
+++ Models/Onibus.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace BhBusMetropApi.Models;
+
+public class Onibus
+{
+    [Key]
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public int Numero { get; set; }
+    public string Cor { get; set; }
+    public string Tipo { get; set; }
+    public double Peso { get; set; }
+
+}
